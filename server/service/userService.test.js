05c4ad58691056657fee24d8db4b2c98efdfab1a
RunningLearner <tkacnsdms@naver.com>
2023-02-26 16:39:50 +0900

    refact: 객체 생성후 테스트하도록 수정

--- server/service/userService.test.js
+++ server/service/userService.test.js
@@ -1,47 +1,49 @@
-// userService.test.js
-import signUp from "./userService.js";
 import { jest, expect, it } from "@jest/globals";
-import { User } from "../models/index.js";
+import UserService from "../service/userService";
+import { User } from "../models";
 
-describe("signUp function", () => {
-  const email = "test@example.com";
-  const password = "password";
-  const name = "Test User";
+describe("UserService", () => {
+  let userService;
 
-  it("should create a new user when email is not taken", async () => {
-    User.findOne = jest.fn(() => null);
-    User.create = jest.fn((email, password, name) => {
-      return {
-        result: "회원가입이 완료되었습니다. 로그인을 해주세요.",
-      };
-    });
-    const result = await signUp(email, password, name);
-
-    expect(User.findOne).toHaveBeenCalledWith({ email });
-    expect(User.create).toHaveBeenCalledWith({
-      email,
-      password: expect.any(String),
-      name,
-    });
-    expect(result).toEqual({
-      result: "회원가입이 완료되었습니다. 로그인을 해주세요.",
-    });
+  beforeEach(() => {
+    userService = new UserService();
   });
 
-  it("should throw an error when email is already taken", async () => {
-    //when
-    User.findOne = jest.fn((email) => "무언가 찾아진 값");
-    User.create = jest.fn((email, password, name) => {
-      return {
-        result: "회원가입이 완료되었습니다. 로그인을 해주세요.",
-      };
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe("signUp", () => {
+    it("should throw an error if the email is already taken", async () => {
+      User.findOne = jest.fn().mockResolvedValue({ email: "test@test.com" });
+
+      await expect(
+        userService.signUp("test@test.com", "password", "John")
+      ).rejects.toThrow("이미 가입된 이메일입니다.500");
     });
 
-    //then
-    await expect(signUp(email, password, name)).rejects.toThrow(
-      new Error("이미 가입된 이메일입니다.500")
-    );
-    expect(User.findOne).toHaveBeenCalledWith({ email });
-    expect(User.create).not.toHaveBeenCalled();
+    it("should create a new user if the email is available", async () => {
+      User.findOne = jest.fn(() => null);
+      User.create = jest.fn((email, password, name) => {
+        return {
+          result: "회원가입이 완료되었습니다. 로그인을 해주세요.",
+        };
+      });
+
+      const result = await userService.signUp(
+        "test@test.com",
+        "password",
+        "John"
+      );
+
+      expect(User.create).toHaveBeenCalledWith({
+        email: "test@test.com",
+        password: expect.any(String),
+        name: "John",
+      });
+      expect(result).toEqual({
+        result: "회원가입이 완료되었습니다. 로그인을 해주세요.",
+      });
+    });
   });
 });
