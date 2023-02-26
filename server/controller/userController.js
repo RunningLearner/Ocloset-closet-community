53a2b7a3040db7c06b6295a9dee68aa6ae5ccc10
RunningLearner <tkacnsdms@naver.com>
2023-02-26 10:38:12 +0900

    fix: 불필요한 함수 제거

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,9 +1,5 @@
 import userService from "../service/userService.js";
 
-const passwordHash = (password) => {
-  return cryto.createHash("sha1").update(password).digest("hex");
-};
-
 export default async function signUp(req, res, next) {
   try {
     const { email, password, name } = req.body;
