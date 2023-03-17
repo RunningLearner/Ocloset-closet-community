0a1928a8b98f55db26c9abc3817d24d3abe7298e
RunningLearner <tkacnsdms@naver.com>
2023-03-17 22:52:08 +0900

    fix: 제네릭으로 타입을 정해서 내보내기

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
-import PostSchema from "./schemas/post";
-import UserSchema from "./schemas/user";
-import UpmentSchema from "./schemas/upment";
+import PostSchema, { IPost } from "./schemas/post";
+import UserSchema, { IUser } from "./schemas/user";
+import UpmentSchema, { IUpment } from "./schemas/upment";
 import DownmentSchema from "./schemas/downment";
 import LikeSchema from "./schemas/like";
 import DislikeSchema from "./schemas/dislike";
 
-export const Post = mongoose.model("Post", PostSchema);
-export const User = mongoose.model("User", UserSchema);
-export const Upment = mongoose.model("Upment", UpmentSchema);
+export const Post = mongoose.model<IPost>("Post", PostSchema);
+export const User = mongoose.model<IUser>("User", UserSchema);
+export const Upment = mongoose.model<IUpment>("Upment", UpmentSchema);
 export const Downment = mongoose.model("Downment", DownmentSchema);
 export const Like = mongoose.model("Like", LikeSchema);
 export const Dislike = mongoose.model("Dislike", DislikeSchema);
